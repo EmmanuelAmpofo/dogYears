2e15119454d7c32fd08013da4a8be24850e174bc
PoffGenius <88441557+PoffGenius@users.noreply.github.com>
2021-08-29 22:28:55 +0000

    dog years project

---
+++ main.js
@@ -0,0 +1,24 @@
+let myAge = 34
+
+
+let earlyYears = 2
+earlyYears *= 10.5;
+earlyYears = earlyYears
+
+myAge -= 2
+
+
+let laterYears = myAge
+
+
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
+
+myAgeInDogYears = laterYears + earlyYears
+console.log(myAgeInDogYears)
+
+myName = 'POFF'.toLowerCase()
+console.log(myName)
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
